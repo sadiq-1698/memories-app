b2cf3cbef5422068f3b46d41c0efe4cac4f4376b
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-11-11 13:26:00 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
+import memories from './images/memories.png';
 const App = () => {
     return (
-        <div>
-            <h1>Memories App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">
+                    Memories App
+                </Typography>
+                <img src={memories} alt="memories" height="60"></img>
+            </AppBar>
+        </Container>
     )
 }
 
